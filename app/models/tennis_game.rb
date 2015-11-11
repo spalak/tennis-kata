dbf2d9bef999a147652f0838bcd4084d90d7808f
Syam Palakurthy <syampalakurthy@gmail.com>
2015-11-10 23:01:35 -0800

    Attempt to "extract refactor" score method

--- app/models/tennis_game.rb
+++ app/models/tennis_game.rb
@@ -34,21 +34,41 @@ class TennisGame < ActiveRecord::Base
 	def score
 		p1_tenScore = translate(@p1_points)
 		p2_tenScore = translate(@p2_points)
+		
 		if (@p1_points < 3 && @p2_points < 3)
-			"#{p1_tenScore}-#{p2_tenScore}"
-		elsif ((@p1_points - @p2_points)==1 && @p1_points > 3)
-			"Advantage In"
-		elsif ((@p2_points - @p1_points)==1 && @p2_points > 3)
-			"Advantage Out"
+			score_before_deuce_or_defeat(p1_tenScore, p2_tenScore)
+		elsif ((@p1_points - @p2_points)==1 && @p1_points > 3) || ((@p2_points - @p1_points)==1 && @p2_points > 3)
+			score_after_deuce(@p1_points, @p2_points)
 		elsif (@p1_points == @p2_points && @p1_points >= 3)
 			"Deuce"
-		elsif (@p1_points >= 4)
-			"#{@player_one} defeats #{@player_two}"
-		elsif (@p2_points >= 4)
-			"#{@player_two} defeats #{@player_one}"
+		elsif (@p1_points >= 4 || @p2_points >= 4)
+			score_after_victory(@p1_points, @p2_points)
 		elsif ((@p1_points == 3 || @p2_points == 3) && @p1_points != @p2_points)
-			"#{p1_tenScore}-#{p2_tenScore}"
+			score_before_deuce_or_defeat(p1_tenScore, p2_tenScore)
 		end
+	end
+
+	def score_before_deuce_or_defeat(p1_tenScore, p2_tenScore)
+		"#{p1_tenScore}-#{p2_tenScore}"
+	end
+
+	def score_after_deuce(p1_score, p2_score)
+		if (p1_score.to_i > p2_score.to_i)
+			"Advantage In"
+		else
+			"Advantage Out"
+		end
+	end
+
+	def score_after_victory(p1_score, p2_score)
+		if (p1_score > p2_score)
+			"#{@player_one} defeats #{@player_two}"
+		else
+			"#{@player_two} defeats #{@player_one}"
+		end
+	end
+
+	def score_one_player_40
 	end
 
 
